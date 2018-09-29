3c12aba5a0c658a01f8dfcec15df74b417f2d84c
Jan Kenith Johannessen <jankenith.johannessen@gmail.com>
2018-09-29 12:10:13 +0800

    Added simple homepage package/domain and refactored Server

--- main.go
+++ main.go
@@ -1,56 +1,35 @@
 package main
 
 import (
-	"crypto/tls"
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/go-chi/chi"
 
+	"github.com/BottleneckStudio/go-web-starter-kit/homepage"
 	"github.com/BottleneckStudio/go-web-starter-kit/server"
 )
 
 func main() {
+	//Declare Config
 	logger := log.New(os.Stdout, "DEBUG: ", log.LstdFlags|log.Lshortfile)
+	router := chi.NewMux()
+	serverAddress := ":1437"
+
+	//Create new Server
 	serverConfig := server.Config{
 		Server: &server.Server{
-			Logger: logger,
-			Router: chi.NewRouter(),
-			TLSConfig: &tls.Config{
-				// Causes servers to use Go's default ciphersuite preferences,
-				// which are tuned to avoid attacks. Does nothing on clients.
-				PreferServerCipherSuites: true,
-				// Only use curves which have assembly implementations
-				CurvePreferences: []tls.CurveID{
-					tls.CurveP256,
-					tls.X25519, // Go 1.8 only
-				},
-				MinVersion: tls.VersionTLS12,
-				CipherSuites: []uint16{
-					tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-					tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-					tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305, // Go 1.8 only
-					tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,   // Go 1.8 only
-					tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-					tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-
-					// Best disabled, as they don't provide Forward Secrecy,
-					// but might be necessary for some clients
-					// tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-					// tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
-				},
-			},
-			ServerAddress: ":1437",
+			Logger:        logger,
+			Router:        router,
+			ServerAddress: serverAddress,
 		},
 	}
-
 	srv := server.New(&serverConfig)
 
-	srv.Router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello world"))
-	})
+	//Setup Domain specific routes
+	h := homepage.New(srv.Logger)
+	h.SetupRoutes(srv.Router)
 
+	//Start Server
 	srv.Start()
-	// fmt.Println("vim-go")
 }
